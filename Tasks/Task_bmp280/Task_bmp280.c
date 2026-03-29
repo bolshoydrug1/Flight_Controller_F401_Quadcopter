541d99edeb138d591b8579b7f394a65c395b30e4
Beregnoy Konstanten <beregnoy.konstanten@gmail.com>
2026-03-29 07:31:38 +0000

    refact: add new folder for new modules

---
+++ Tasks/Task_bmp280/Task_bmp280.c
@@ -0,0 +1,10 @@
+/*
+ * Task_bmp280.c
+ *
+ *  Created on: Mar 29, 2026
+ *      Author: konstantin1
+ */
+
+
+
+
---
+++ Tasks/Task_bmp280/Task_bmp280.h
@@ -0,0 +1,15 @@
+/*
+ * Task_bmp280.h
+ *
+ *  Created on: Mar 29, 2026
+ *      Author: konstantin1
+ */
+
+#ifndef TASK_BMP280_TASK_BMP280_H_
+#define TASK_BMP280_TASK_BMP280_H_
+
+
+
+
+
+#endif /* TASK_BMP280_TASK_BMP280_H_ */
